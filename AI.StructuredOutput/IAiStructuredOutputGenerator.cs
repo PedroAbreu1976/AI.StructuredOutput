1a9e0d25fde6c9ca69d69cd59081978e38dea706
Pedro Abreu <pedro.abreu@visma.com>
2025-05-13 14:58:18 +0000

    comments

--- AI.StructuredOutput/IAiStructuredOutputGenerator.cs
+++ AI.StructuredOutput/IAiStructuredOutputGenerator.cs
@@ -1,7 +1,17 @@
 ﻿namespace AI.StructuredOutput
 {
+    /// <summary>
+    /// Generator for structured outputs
+    /// </summary>
     public interface IAiStructuredOutputGenerator
     {
+        /// <summary>
+        /// Queries the AI for a structured output
+        /// </summary>
+        /// <typeparam name="TResult">The expected result type</typeparam>
+        /// <param name="question">The question</param>
+        /// <param name="files">Files to be uploaded</param>
+        /// <returns>The structured output</returns>
         Task<TResult?> AskAsync<TResult>(string question, params FileInfo[] files);
     }
 }
--- AI.StructuredOutput/ServiceCollectionExtensions.cs
+++ AI.StructuredOutput/ServiceCollectionExtensions.cs
@@ -7,6 +7,14 @@ namespace AI.StructuredOutput
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Register gemini as the AI to handle structured outputs
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <param name="url">The gemini Url (default: 'https://generativelanguage.googleapis.com/v1beta/models'</param>
+        /// <param name="model">The gemini model (default: 'gemini-1.5-flash')</param>
+        /// <param name="apiKey">Your gemini api key</param>
+        /// <returns>The service collection</returns>
         public static IServiceCollection AddGeminiStructuredOutputGenerator(this IServiceCollection services,
             string url,
             string model,
@@ -31,6 +39,12 @@ namespace AI.StructuredOutput
             return services;
         }
 
+        /// <summary>
+        /// Register gemini as the AI to handle structured outputs
+        /// </summary>
+        /// <param name="services">The service collection</param>
+        /// <param name="apiKey">Your gemini api key</param>
+        /// <returns>The service collection</returns>
         public static IServiceCollection AddGeminiStructuredOutputGenerator(this IServiceCollection services,
             string apiKey)
         {
